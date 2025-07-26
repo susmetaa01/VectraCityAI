88280d2475df633fce157e49f472d1f171d5f094
sa003 <susmeta_a@intuit.com>
2025-07-26 21:10:18 +0530

    updated gnews updation

--- ingest/google_news_rss_publisher.py
+++ ingest/google_news_rss_publisher.py
@@ -1,5 +1,13 @@
-import json
 import os
+import sys
+
+
+current_script_dir = os.path.dirname(os.path.abspath(__file__))
+project_root_dir = os.path.abspath(os.path.join(current_script_dir, os.pardir))
+
+sys.path.insert(0, project_root_dir)
+
+import json
 import time  # For sleeping between polls
 from datetime import datetime, timedelta
 from typing import List
@@ -9,10 +17,10 @@ from google import genai
 from google.cloud import pubsub_v1  # For publishing to Pub/Sub
 from google.genai import types
 
-from ingest.gnews_prompts import get_google_news_prompt, get_structured_news_prompt
 from src.model.incoming_events import AnalysisResponse
 
 # --- Configuration ---
+load_dotenv()
 GCP_PROJECT_ID = os.getenv('GCP_PROJECT_ID', 'schrodingers-cat-466413')
 PUBSUB_TOPIC_GNEWS_RAW_NEWS = os.getenv('PUBSUB_TOPIC_GNEWS_RAW_NEWS', 'gnews-incoming-raw-events') # NEW Pub/Sub topic for news
 
@@ -32,10 +40,30 @@ current_time = datetime.now()
 
 area = ["bangalore"]
 news_tags = ["traffic, floods, power cut, bbmp, municipality, drainage, protests, accidents, rallies, events"]
-news_timedelta_days = 10
+news_timedelta_days = 40
 client = genai.Client()
 
-load_dotenv()
+
+
+def get_google_news_prompt(start_time, end_time, area, tags):
+    return f"""
+    You are an expert in news analysis. Your task is to analyze news articles and extract relevant information.
+    Find news between {start_time} and {end_time} relating to areas = {area} in India, and extract the following, relating to tags={tags}:
+    1. Summarise the news.
+    2. Capture the last updated time or published time of the news with date.
+    3. If the new has area and sublocation, capture it. If it has latitude and longitude capture that too.
+    3. Capture the source of the news.
+    
+    !!NOTE: If there is no news strictly between the timestamps mentioned, just return "NO_NEWS". Dont add news published
+    outside the time range given above.
+    """
+
+def get_structured_news_prompt(news):
+    return f"""
+    Given the following news in form of pointers, structure the text in required format, extracting key informations.
+    News pointers: \n
+    {news}
+    """
 
 
 def parse_google_news():
@@ -83,9 +111,9 @@ def fetch_and_publish_feed():
             pass
         for res in responses:
             # Publish to Pub/Sub
-            json_payload = json.dumps(res, ensure_ascii=False)  # ensure_ascii=False for proper unicode
+            json_payload = res.model_dump_json(exclude_none=True)
             future = publisher.publish(PUBSUB_TOPIC_PATH, json_payload.encode('utf-8'))
-            print(f"Published {res} new articles.")
+            print(f"Published {future} new articles.")
     except Exception as e:
         print(f"Error fetching or processing feed {current_time}: {e}")
 
--- src/vectra_city_pipeline.py
+++ src/vectra_city_pipeline.py
@@ -90,11 +90,15 @@ def run_pipeline():
         google_news_pipeline = (
                 pipeline
                 | 'ReadRawGoogleNewsFeed' >> io_connectors.ReadGoogleNewsFeed()
-                | 'DecodeAndParseNewsJson' >> beam.Map(lambda element: json.loads(element.decode('utf-8')))
-                | 'PrintParsedNewsArticle' >> beam.Map(lambda x: print(f"Parsed News Article: {x}")) # Debug parsed
-                # | 'ComprehendNewsArticle' >> beam.ParDo(data_normaliser.ComprehendFn())
-                # | 'AnalyzeNewsArticlesWithGemini' >> beam.ParDo(gemini_analyzer.AIComprehensionFn())
-                # | 'PrintAnalyzedNewsArticle' >> beam.Map(lambda x: print(f"Analyzed News Article: {x}"))
+                | 'DecodeAndParseNewsJson' >> beam.Map(
+            lambda element: json.loads(element.decode('utf-8')))
+                | 'PrintParsedNewsArticle' >> beam.Map(lambda x: print(f"Parsed News Article: {x}"))
+                | 'WriteGnewsAnalyzedToBigQuerySql' >> beam.ParDo(BigQuerySqlInsertFn()
+                                                                     )
+        # Debug parsed
+            # | 'ComprehendNewsArticle' >> beam.ParDo(data_normaliser.ComprehendFn())
+            # | 'AnalyzeNewsArticlesWithGemini' >> beam.ParDo(gemini_analyzer.AIComprehensionFn())
+            # | 'PrintAnalyzedNewsArticle' >> beam.Map(lambda x: print(f"Analyzed News Article: {x}"))
         )
 
         # --- Trigger Event Listener (Separate Branch, No AI Analysis on triggers) ---
